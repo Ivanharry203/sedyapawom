3db44f05afeb43df6a67241fd8317e02ea552b9d
Harana <ivanharry203@gmail.com>
2024-10-30 20:57:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openWhatsApp() {
     const phoneNumber = "62895603126471";
-    const message = "Hallo Pawon Catering, mau tanya-tanya soal pemesanan";
+    const message = "Hallo Pawon Catering";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, "_blank");
 }
